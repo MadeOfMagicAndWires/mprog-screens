c6e13b40d1ce64eb1c61e229f2a17730bdbcd136
Joost Bremmer <toost.b@gmail.com>
2015-11-12 22:56:06 +0100

    Work on layouts and make MainActivity's action button refer to the second activity

--- app/src/main/java/nl/mprog/screens/MainActivity.java
+++ app/src/main/java/nl/mprog/screens/MainActivity.java
@@ -1,5 +1,6 @@
 package nl.mprog.screens;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,14 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                Intent storyactivity = new Intent(getBaseContext(), StoryActivity.class);
+                startActivity(storyactivity);
             }
         });
     }
