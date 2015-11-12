960984dbb5794a1708363d49f98f78ffa3cf7dff
Joost Bremmer <toost.b@gmail.com>
2015-11-12 04:17:50 +0100

    Preperations for a Second Activity
    + Added StoryActivity to AndroidManifest.xml
    + Created StoryActivity.java
    + Created activity_story.xml
    + set FloatingActionButton in MainActivity to finish()

---
+++ app/src/main/java/nl/mprog/screens/StoryActivity.java
@@ -0,0 +1,28 @@
+package nl.mprog.screens;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Story Activity Class
+ */
+public class StoryActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_story);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+
+            }
+        });
+    }
+}
